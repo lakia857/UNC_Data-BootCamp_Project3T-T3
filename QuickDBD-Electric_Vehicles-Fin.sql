d7f96084433794d21e075079d9b7c1c912c8eea9
Lakia White <lakiawhite@rempacntnet.com>
2024-04-07 03:19:57 -0400

    QuickDB

---
+++ QuickDBD-Electric_Vehicles-Fin.sql
@@ -0,0 +1,65 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/BVF6uY
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "cleaned_ev_stations" (
+    "Station_Name" VARCHAR(250),
+    "Street_Address" VARCHAR(250),
+    "City" VARCHAR(50),
+    "State" VARCHAR(2),
+    "ZIP" VARCHAR(10),
+    "region" VARCHAR(50),
+    "Groups_With_AccessCode" VARCHAR(250),
+    "Access_Days_Time" VARCHAR(500),
+    "Latitude" FLOAT,
+    "Longitude" FLOAT,
+    "EV_Pricing" VARCHAR(500),
+    CONSTRAINT "pk_cleaned_ev_stations" PRIMARY KEY (
+        "region"
+     )
+);
+
+CREATE TABLE "gas_prices_df" (
+    "year" INT,
+    "Jan" Decimal,
+    "Feb" Decimal,
+    "Mar" Decimal,
+    "Apr" Decimal,
+    "May" Decimal,
+    "Jun" Decimal,
+    "Jul" Decimal,
+    "Aug" Decimal,
+    "Sep" Decimal,
+    "Oct" Decimal,
+    "Nov" Decimal,
+    "Dec" Decimal,
+    CONSTRAINT "pk_gas_prices_df" PRIMARY KEY (
+        "year"
+     )
+);
+
+CREATE TABLE "ev_sales" (
+    "region" VARCHAR(50),
+    "powertrain" VARCHAR(5),
+    "year" INT,
+    "value" Decimal,
+    CONSTRAINT "pk_ev_sales" PRIMARY KEY (
+        "region"
+     )
+);
+
+ALTER TABLE "cleaned_ev_stations" ADD CONSTRAINT "fk_cleaned_ev_stations_region" FOREIGN KEY("region")
+REFERENCES "ev_sales" ("region");
+
+ALTER TABLE "ev_sales" ADD CONSTRAINT "fk_ev_sales_year" FOREIGN KEY("year")
+REFERENCES "gas_prices_df" ("year");
+
+ALTER TABLE cleaned_ev_stations DROP CONSTRAINT pk_cleaned_ev_stations;
+
+ALTER TABLE ev_sales DROP CONSTRAINT pk_ev_sales;
+
+SELECT * FROM cleaned_ev_stations
---
+++ QuickDBD-Electric_Vehicles.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/BVF6uY
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "cleaned_ev_stations" (
+    "Station_Name" VARCHAR(100)   NOT NULL,
+    "Street_Address" INT   NOT NULL,
+    "City" VARCHAR(50)   NOT NULL,
+    "State" VARCHAR(2)   NOT NULL,
+    "ZIP" VARCHAR(5)   NOT NULL,
+    "region" VARCHAR(50)   NOT NULL,
+    "Groups_With_AccessCode" VARCHAR(75)   NOT NULL,
+    "Access_Days_Time" VARCHAR(75)   NOT NULL,
+    "Latitude" INT   NOT NULL,
+    "Longitude" INT   NOT NULL,
+    "EV_Pricing" VARCHAR(75)   NOT NULL,
+    CONSTRAINT "pk_cleaned_ev_stations" PRIMARY KEY (
+        "region"
+     )
+);
+
+CREATE TABLE "gas_prices_df" (
+    "year" INT   NOT NULL,
+    "Jan" INT   NOT NULL,
+    "Feb" INT   NOT NULL,
+    "Mar" INT   NOT NULL,
+    "Apr" INT   NOT NULL,
+    "May" INT   NOT NULL,
+    "Jun" INT   NOT NULL,
+    "Jul" INT   NOT NULL,
+    "Aug" INT   NOT NULL,
+    "Sep" INT   NOT NULL,
+    "Oct" INT   NOT NULL,
+    "Nov" INT   NOT NULL,
+    "Dec" INT   NOT NULL,
+    CONSTRAINT "pk_gas_prices_df" PRIMARY KEY (
+        "year"
+     )
+);
+
+CREATE TABLE "ev_sales" (
+    "region" VARCHAR(50)   NOT NULL,
+    "powertrain" VARCHAR(5)   NOT NULL,
+    "year" INT   NOT NULL,
+    "value" INT   NOT NULL,
+    CONSTRAINT "pk_ev_sales" PRIMARY KEY (
+        "region"
+     )
+);
+
+ALTER TABLE "cleaned_ev_stations" ADD CONSTRAINT "fk_cleaned_ev_stations_region" FOREIGN KEY("region")
+REFERENCES "ev_sales" ("region");
+
+ALTER TABLE "ev_sales" ADD CONSTRAINT "fk_ev_sales_year" FOREIGN KEY("year")
+REFERENCES "gas_prices_df" ("year");
+
